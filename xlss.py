49496f2595a6a1fe8101500a6dc63fddea2fb168
Shukai Ni <nishukai@hotmail.com>
2024-05-05 15:27:00 -0400

    Prediction by batch

--- xlss.py
+++ xlss.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 def load_image(file_path):
     img = tf.io.read_file(file_path)
     img = tf.image.decode_png(img, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.convert_image_dtype(img, tf.float16)
     return img
 
 
@@ -29,18 +29,27 @@ if __name__ == "__main__":
     test_patches = os.listdir(temp_dir)
     test_patches = [os.path.join(temp_dir, file) for file in test_patches]
 
-    test_ds = tf.data.Dataset.from_tensor_slices(test_patches)
-    test_ds = test_ds.map(load_image,
-                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    test_ds = test_ds.batch(128)
-    pred = model.predict(test_ds)
-    pred = np.clip(pred, 0, 1)
+    PATCHES_PER_PREDICTION = 10000
+    for i in range(0, len(test_patches), PATCHES_PER_PREDICTION):
+        test_ds = tf.data.Dataset.from_tensor_slices(
+            test_patches[i:i + PATCHES_PER_PREDICTION])
+        test_ds = test_ds.map(load_image,
+                              num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        test_ds = test_ds.batch(32).prefetch(
+            buffer_size=tf.data.experimental.AUTOTUNE)
+        pred = model.predict(test_ds)
+        pred = np.clip(pred, 0, 1)
 
-    # Overwrite the test_patches
-    for file_path, img in zip(test_patches, pred):
-        img = tf.image.convert_image_dtype(img, tf.uint8)
-        img = tf.image.encode_png(img)
-        tf.io.write_file(file_path, img)
+        # Overwrite the test_patches
+        for file_path, img in zip(test_patches[i:i + PATCHES_PER_PREDICTION],
+                                  pred):
+            img = tf.image.convert_image_dtype(img, tf.uint8)
+            img = tf.image.encode_png(img)
+            tf.io.write_file(file_path, img)
+
+        tf.keras.backend.clear_session()
+
+    print("Stitching images back together...")
 
     ImageStitcher(temp_dir, opath)()
 
